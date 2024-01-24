d2fbd4b13b9de6c46042acc6e44474a15103e651
VVA0 <djvanila@bk.ru>
2024-01-24 10:17:49 +0300

    color block

---
+++ src/components/ColorBlock.vue
@@ -0,0 +1,59 @@
+<template>
+  <div :class="classBlock" class="color-block">1</div>
+</template>
+
+<script>
+export default {
+  name: "ColorBlock",
+  props: [
+    "backgroundBlue",
+    "backgroundRed",
+    "backgroundGreen",
+    "colorBlue",
+    "colorRed",
+    "colorGreen",
+  ],
+  computed: {
+    classBlock() {
+      return {
+        "background-blue": this.backgroundBlue,
+        "background-red": this.backgroundRed,
+        "background-green": this.backgroundGreen,
+        "color-blue": this.colorBlue,
+        "color-red": this.colorRed,
+        "color-green": this.colorGreen,
+      };
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.color-block {
+  width: 20px;
+  height: 20px;
+  border-radius: 5px;
+  border: 1px solid black;
+  transition: all 0.5s ease;
+  background-color: white;
+  &.background-blue {
+    //Так я добавляю дополнительный класс
+    background-color: blue;
+  }
+  &.background-red {
+    background-color: red;
+  }
+  &.background-green {
+    background-color: green;
+  }
+  &.color-blue {
+    color: blue;
+  }
+  &.color-red {
+    color: red;
+  }
+  &.color-green {
+    color: green;
+  }
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="home">
+    <ColorBlock
+      :background-blue="isBackgroundBlue"
+      :background-green="isBackgroundGreen"
+      :background-red="isBackgroundRed"
+      :color-blue="isColorBlue"
+      :color-green="isColorGreen"
+      :color-red="isColorRed"
+    />
+  </div>
+</template>
+
+<script>
+import ColorBlock from "@/components/ColorBlock.vue";
+
+export default {
+  name: "HomeView",
+  components: {
+    ColorBlock,
+  },
+  data() {
+    return {
+      backgroundIndex: 0,
+      colorIndex: 1,
+    };
+  },
+  computed: {
+    isBackgroundBlue() {
+      return this.backgroundIndex === 0;
+    },
+    isBackgroundRed() {
+      return this.backgroundIndex === 1;
+    },
+    isBackgroundGreen() {
+      return this.backgroundIndex === 2;
+    },
+    isColorBlue() {
+      return this.colorIndex === 0;
+    },
+    isColorRed() {
+      return this.colorIndex === 1;
+    },
+    isColorGreen() {
+      return this.colorIndex === 2;
+    },
+  },
+  mounted() {
+    setInterval(() => {
+      if (this.backgroundIndex >= 2) {
+        this.backgroundIndex = 0;
+      } else {
+        this.backgroundIndex += 1;
+      }
+      if (this.colorIndex >= 2) {
+        this.colorIndex = 0;
+      } else {
+        this.colorIndex += 1;
+      }
+    }, 2000);
+  },
+};
+</script>
